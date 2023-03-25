645e1579b7c211c745c7de7ce83b1133c494f35b
Afrikhana <brianmurigi64@gmail.com>
2023-03-25 16:17:18 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,51 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
-}
 
-function ternaryCheckCity(){
+function scuberGreetingForFeet(ride){
   // Write your code here!
-}
+   let result;
+  if (ride<=400){
+     result="This one is on me!";
+  } 
+      else if (ride>2000 || ride<=2499){
+         result="I will gladly take your thirty bucks.";
+      }
+     else {
+          result="No can do."
+        } 
+        return result;
+      }
+       
+       
+        
+    
+    function ternaryCheckCity(truth){
+//   // Write your code here!
+let city="NYC"
+let ss="NYC";
+     if(city==ss){
+      truth="Ok, sounds good."
+     }
+     else if(city!=ss){
+      truth="No go."
+     }
+   
+
+     return truth;
+ }
 
 function switchOnCharmFromTip(){
-  // Write your code here!
-}
+   // Write your code here!
+   let tip1=300;
+   
+   let greet;
+    switch(tip1){
+      case 300:
+        greet="Thank you so much."
+        break; 
+        case 200:
+        greet="Thank you."
+        break; 
+        default:
+          console.log('bye')
+    }
+    return greet;
+ }
